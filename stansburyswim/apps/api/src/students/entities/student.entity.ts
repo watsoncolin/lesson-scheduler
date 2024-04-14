33005bca8a32c97e9ea3c3892565fbb497221c85
watsoncolin <colin@colinwatson.me>
2024-04-14 15:08:08 -0600

    before ddd script

--- stansburyswim/apps/api/src/students/entities/student.entity.ts
+++ stansburyswim/apps/api/src/students/entities/student.entity.ts
@@ -1,1 +1,29 @@
-export class Student {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema({
+  collection: 'students',
+  timestamps: true,
+})
+export class StudentEntity extends Document {
+  _id: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+
+  @Prop({ required: true })
+  userId: Types.ObjectId;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  birthday: Date;
+
+  @Prop({ required: true })
+  ability: string;
+
+  @Prop({ required: true })
+  notes: string;
+}
+
+export const StudentSchema = SchemaFactory.createForClass(StudentEntity);

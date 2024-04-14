593ed1cc444849ba24117e964818641d2b36835b
watsoncolin <colin@colinwatson.me>
2024-04-14 15:08:08 -0600

    before ddd script

--- stansburyswim/apps/api/src/app/app.module.ts
+++ stansburyswim/apps/api/src/app/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  StudentEntity,
+  StudentSchema,
+} from '../students/entities/student.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27019/stansburyswim'),
+    MongooseModule.forFeature([
+      // { name: 'User', schema: UserSchema },
+      { name: StudentEntity.name, schema: StudentSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

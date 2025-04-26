6cf0b809456476490c0ac5b63c6307da6a66e2c0
watsoncolin <colin@colinwatson.me>
2025-04-26 15:36:42 -0400

    another try

--- apps/api/src/file/file.service.ts
+++ apps/api/src/file/file.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@nestjs/common'
-import { Storage } from '@google-cloud/storage'
 import { ConfigService } from '@nestjs/config'
+import { Storage } from '@google-cloud/storage'
 import { v4 as uuidv4 } from 'uuid'
 import 'multer'
 
 @Injectable()
 export class FileService {
-  private storage: Storage
-  private bucket: string
+  private readonly storage: Storage
+  private readonly bucketName: string
 
-  constructor(private configService: ConfigService) {
+  constructor(private readonly configService: ConfigService) {
     this.storage = new Storage()
-    this.bucket = this.configService.get<string>('GCP_STORAGE_BUCKET', 'stansburyswim-public')
+    this.bucketName = this.configService.get<string>('GCP_STORAGE_BUCKET') || 'stansburyswim-public'
   }
 
   async uploadFile(file: Express.Multer.File, userId: string): Promise<string> {
+    if (!file || !file.buffer) {
+      throw new Error('File buffer is empty')
+    }
+
     const fileExtension = file.originalname.split('.').pop()
     const fileName = `${userId}/${uuidv4()}.${fileExtension}`
 
-    const bucket = this.storage.bucket(this.bucket)
+    const bucket = this.storage.bucket(this.bucketName)
     const blob = bucket.file(fileName)
 
     await blob.save(file.buffer, {
@@ -27,9 +31,10 @@ export class FileService {
       metadata: {
         contentType: file.mimetype,
       },
+      predefinedAcl: 'publicRead', // optional: make file public immediately
     })
 
-    const publicUrl = `https://storage.googleapis.com/${this.bucket}/${fileName}`
+    const publicUrl = `https://storage.googleapis.com/${this.bucketName}/${fileName}`
     return publicUrl
   }
 }

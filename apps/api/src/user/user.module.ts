980fe0ec020a5fe7364e2e68fb5e285b2c74bf28
watsoncolin <colin@colinwatson.me>
2025-05-01 00:53:38 -0400

    fix dep

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -5,8 +5,9 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { UserEntity, UserSchema } from './entities/user.entity'
 import { MeController } from './me.controller'
 import { StudentModule } from '../student/student.module'
+import { CqrsModule } from '@nestjs/cqrs'
 @Module({
-  imports: [MongooseModule.forFeature([{ name: UserEntity.name, schema: UserSchema }]), StudentModule],
+  imports: [MongooseModule.forFeature([{ name: UserEntity.name, schema: UserSchema }]), StudentModule, CqrsModule],
   // Register MeController first to avoid routing conflicts with UsersController :id param
   controllers: [MeController, UserController],
   providers: [UserService],

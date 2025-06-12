b26635e48af897224d8bcc0c82ef2e84045e13e3
watsoncolin <colin@colinwatson.me>
2025-06-12 19:29:12 -0400

    fix search

--- apps/api/src/user/dto/user-search-request.dto.ts
+++ apps/api/src/user/dto/user-search-request.dto.ts
@@ -1,15 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsOptional, IsString, IsNumber } from 'class-validator'
+import { Type } from 'class-transformer'
 
 export class UserSearchRequestDto {
   @ApiProperty({ required: false, default: 1 })
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
   page?: number = 1
 
   @ApiProperty({ required: false, default: 50 })
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
   limit?: number = 50
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
   name?: string
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
   phone?: string
 }

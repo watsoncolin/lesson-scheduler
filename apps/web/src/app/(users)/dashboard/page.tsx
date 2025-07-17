1290f311e95fea3dd8dd321ca754c48aa304718a
watsoncolin <colin@colinwatson.me>
2025-07-17 17:55:09 -0400

    catch exceptions

--- apps/web/src/app/(users)/dashboard/page.tsx
+++ apps/web/src/app/(users)/dashboard/page.tsx
@@ -8,13 +8,30 @@ import { ScheduleService } from '@/services/api/shared/scheduleService'
 import { PoolService } from '@/services/api/shared/poolService'
 import { InstructorService } from '@/services/api/shared/instructorService'
 import Header from './components/Header'
+import { ApiError } from '@/api/core/ApiError'
+import { redirect } from 'next/navigation'
+import { PoolDto, ScheduleResponseDto, InstructorResponseDto } from '@/api'
 
 export default async function Index() {
-  const response = await TransactionsService.getMyCreditBalance()
-  const credits = response.balances.find((b: any) => b.creditType === 'private')?.balance ?? 0
-  const schedules = await ScheduleService.findMySchedule()
-  const pools = await PoolService.findAll()
-  const instructors = await InstructorService.findAll()
+  // catch errors and redirect to sign-in if they are 401
+  let credits = 0
+  let schedules: ScheduleResponseDto[] = []
+  let pools: PoolDto[] = []
+  let instructors: InstructorResponseDto[] = []
+  try {
+    const response = await TransactionsService.getMyCreditBalance()
+    credits = response.balances.find((b: any) => b.creditType === 'private')?.balance ?? 0
+    schedules = await ScheduleService.findMySchedule()
+    pools = await PoolService.findAll()
+    instructors = await InstructorService.findAll()
+  } catch (error) {
+    if (error instanceof ApiError && error.status === 401) {
+      redirect('/sign-in')
+    } else {
+      throw error
+    }
+  }
+
   const stats = [
     {
       name: 'Available lesson credits',
